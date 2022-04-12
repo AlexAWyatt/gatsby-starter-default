e6e6dd0ea955410ab6b3ef094169b15f594956c8
AlexAWyatt <alex.wyatt20@gmail.com>
2022-04-11 23:43:20 -0400

    added links to all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ const IndexPage = () => {
     <main>
       <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/receptionist_ui">Receptionist UI</Link>
+      <Link to="/dentist-hygienist_ui">Dentist/Hygienist UI</Link>
       <Link to="/patient_ui">Patient UI</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
